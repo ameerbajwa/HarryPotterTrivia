aeef90c5b4c539f2edac3d242e9a7cc5f220e82c
Ameer Bajwa <ameerbajwa@gmail.com>
2020-08-19 07:00:00 -0700

    Adding a background image to the home page view controller

--- HarryPotterTrivia/Controllers/HomePageViewController.swift
+++ HarryPotterTrivia/Controllers/HomePageViewController.swift
@@ -15,11 +15,26 @@ class HomePageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        assignBackgroundImage()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
 
     }
+    
+    func assignBackgroundImage(){
+          let background = UIImage(named: "HarryPotterBackgroundImage")
+
+          var imageView : UIImageView!
+          imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+          imageView.clipsToBounds = true
+          imageView.image = background
+          imageView.center = view.center
+          view.addSubview(imageView)
+          self.view.sendSubviewToBack(imageView)
+      }
     
 
     /*
