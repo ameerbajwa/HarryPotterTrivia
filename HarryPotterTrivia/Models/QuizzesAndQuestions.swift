6295fe1fa1fc82996e3f850596b576452cef2926
Ameer Bajwa <ameerbajwa@gmail.com>
2020-08-22 15:26:35 -0700

    Created a gitignore file to hide the quizzes and questions model file where I will hardcode the some of the quizzes and questions first before I create a database.

---
+++ HarryPotterTrivia/Models/QuizzesAndQuestions.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzesAndQuestions.swift
+//  HarryPotterTrivia
+//
+//  Created by Ameer Bajwa on 8/22/20.
+//  Copyright © 2020 Ameer Bajwa. All rights reserved.
+//
+
+import Foundation
+
+class Quizzes {
+    
+}
+
+class Questions {
+    
+}
