16b3fc0aee060e627f57901292fb4126a39c9b6c
Ameer Bajwa <ameerbajwa@gmail.com>
2020-08-23 16:34:38 -0700

    Using .gitignore file to hide quizzes and questions being hardcoded for project at the moment before I use a database to properly store the data

--- HarryPotterTrivia/Controllers/QuizzesViewController.swift
+++ HarryPotterTrivia/Controllers/QuizzesViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class QuizzesViewController: UIViewController {
+    
+    var HarryPotterQuizNames = Quizzes()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,7 @@ class QuizzesViewController: UIViewController {
         self.navigationItem.hidesBackButton = true
 
         // Do any additional setup after loading the view.
+        print(HarryPotterQuizNames.names)
     }
     
     func assignBackground() {
