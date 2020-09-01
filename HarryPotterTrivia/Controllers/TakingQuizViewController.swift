0466b33a335853fff85764250b33e20c1f342456
Ameer Bajwa <ameerbajwa@gmail.com>
2020-08-31 21:59:42 -0700

    Setting up the quiz page

--- HarryPotterTrivia/Controllers/TakingQuizViewController.swift
+++ HarryPotterTrivia/Controllers/TakingQuizViewController.swift
@@ -13,13 +13,46 @@ class TakingQuizViewController: UIViewController {
     var timer = Timer()
     var question_number: Int = 1
     var quiz: String?
+    var phoneWidth: CGFloat?
+    
+    let parchmentImage = UIImage(named: "CroppedParchmentRollImage")
+    var questionImageView: UIImageView?
+    var choiceAButton = UIButton()
+    var choiceBButton = UIButton()
+    var choiceCButton = UIButton()
+    var choiceDButton = UIButton()
+    
+    var users_answers: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.black
+        phoneWidth = self.view.bounds.size.width
         print(quiz)
+        
+        settingQuestionScreen()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func settingQuestionScreen() {
+        questionImageView = UIImageView(image: parchmentImage)
+        questionImageView?.frame = CGRect(x: 20.0, y: 100.0, width: phoneWidth!-40.0, height: 350.0)
+        
+        settingUpChoiceButtons(button: choiceAButton, y_position: 500.0)
+        settingUpChoiceButtons(button: choiceBButton, y_position: 580.0)
+        settingUpChoiceButtons(button: choiceCButton, y_position: 660.0)
+        settingUpChoiceButtons(button: choiceDButton, y_position: 740.0)
+        
+        self.view.addSubview(questionImageView!)
+    }
+    
+    func settingUpChoiceButtons(button: UIButton, y_position: Double) {
+        button.frame = CGRect(x: 20.0, y: y_position, width: Double(phoneWidth!)-40.0, height: 50.0)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = UIColor(red: 255/255, green: 226/255, blue: 168/255, alpha: 1.0)
+        self.view.addSubview(button)
     }
     
 
